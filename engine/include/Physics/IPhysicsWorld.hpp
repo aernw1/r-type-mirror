6c93faa806e9d4ce9df6132d102e196af7443990
keane <keane.adjobi@epitech.eu>
2025-11-19 15:23:33 +0100

    feat(physics): interfaces with IModule integration

--- engine/include/Physics/IPhysicsWorld.hpp
+++ engine/include/Physics/IPhysicsWorld.hpp
@@ -2,16 +2,22 @@
 
 #include <memory>
 #include <vector>
+#include "Core/Module.hpp"
+#include "Core/Logger.hpp"
 
 namespace Physics {
 
     class RigidBody;
 
-    class IPhysicsWorld {
+    class IPhysicsWorld : public RType::Core::IModule {
     public:
         virtual ~IPhysicsWorld() = default;
 
-        virtual void update(float deltaTime) = 0;
+        virtual const char* GetName() const override = 0;
+        virtual RType::Core::ModulePriority GetPriority() const override = 0;
+        virtual bool Initialize(RType::Core::Engine* engine) override = 0;
+        virtual void Shutdown() override = 0;
+        virtual void Update(float deltaTime) override = 0;
         virtual std::shared_ptr<RigidBody> createRigidBody() = 0;
         virtual void destroyRigidBody(const std::shared_ptr<RigidBody>& body) = 0;
         virtual std::vector<std::shared_ptr<RigidBody>> getRigidBodies() const = 0;
--- engine/include/Physics/PhysicsEngine.hpp
+++ engine/include/Physics/PhysicsEngine.hpp
@@ -2,14 +2,22 @@
 
 #include <memory>
 #include <utility>
+#include "Core/Module.hpp"
+#include "Core/Logger.hpp"
 
 namespace Physics {
 
     class IPhysicsWorld;
 
-    class PhysicsEngine {
+    class PhysicsEngine : public RType::Core::IModule {
     public:
         virtual ~PhysicsEngine() = default;
+
+        virtual const char* GetName() const override = 0;
+        virtual RType::Core::ModulePriority GetPriority() const override = 0;
+        virtual bool Initialize(RType::Core::Engine* engine) override = 0;
+        virtual void Shutdown() override = 0;
+        virtual void Update(float deltaTime) override = 0;
 
         virtual std::shared_ptr<IPhysicsWorld> createWorld() = 0;
         virtual void setGravity(float gx, float gy) = 0;
