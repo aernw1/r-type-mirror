2e0cb5dfab085296afd8d64a163ccc6512bb2aff
Gabriel MCHIRI <gabriel.mchiri@epitech.eu>
2025-11-26 18:23:43 +0100

    feat(ECS/System) : add CollisionSystem header with a methode to check the collision between two box colliders

---
+++ libs/engine/include/ECS/CollisionSystem.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ISystem.hpp"
+
+namespace RType {
+    namespace ECS {
+
+    class CollisionSystem : public ISystem {
+        public:
+            CollisionSystem() = default;
+            ~CollisionSystem() override = default;
+
+            const char* GetName() const override { return "CollisionSystem"; }
+
+            void Update(Registry& registry, float deltaTime) override;
+
+            static bool CheckCollision(Registry& registry, Entity a, Entity b);
+
+        private:
+            static bool CheckAABB(float x1, float y1, float w1, float h1,
+                float x2, float y2, float w2, float h2);
+        };
+    }
+}
