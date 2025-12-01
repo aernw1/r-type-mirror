9806bdb9e99f4b619ccffa1274b277a4ca48c815
Dryss <dryss.margueritte@epitech.eu>
2025-12-01 14:56:04 +0100

    refactor(ecs/player-system): remove player creation logic from PlayerSystem and clean up unused includes

--- libs/engine/include/ECS/PlayerSystem.hpp
+++ libs/engine/include/ECS/PlayerSystem.hpp
@@ -2,8 +2,6 @@
 
 #include "ISystem.hpp"
 #include "Renderer/IRenderer.hpp"
-#include "Math/Types.hpp"
-#include <string>
 
 namespace RType {
 
@@ -17,15 +15,11 @@ namespace RType {
             void Update(Registry& registry, float deltaTime) override;
             const char* GetName() const override { return "PlayerSystem"; }
 
-            static Entity CreatePlayer(Registry& registry, uint8_t playerNumber, uint64_t playerHash,
-                                     float startX, float startY, Renderer::IRenderer* renderer);
             static void ClampPlayerToScreen(Registry& registry, Entity player, float screenWidth = 1280.0f,
                                           float screenHeight = 720.0f);
 
         private:
             Renderer::IRenderer* m_renderer;
-            static Math::Color GetPlayerColor(uint8_t playerNumber);
-            static std::string GetPlayerSpritePath(uint8_t playerNumber);
         };
 
     }
