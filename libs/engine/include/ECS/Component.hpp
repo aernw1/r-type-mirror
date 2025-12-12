922516b42fef9bc8ebe750b737b3ace84fbdcf45
erwan <erwan.seytor@epitech.eu>
2025-12-12 23:00:25 +0100

    feat(ECS): added collision layer filtering and circle collider components

--- libs/engine/include/ECS/Component.hpp
+++ libs/engine/include/ECS/Component.hpp
@@ -184,6 +184,40 @@ namespace RType {
             Obstacle() = default;
             Obstacle(bool isBlocking) : blocking(isBlocking) {}
         };
+
+        struct CollisionLayer : public IComponent {
+            uint16_t layer = 0;      // What layer this entity is on
+            uint16_t mask = 0xFFFF;  // Which layers this entity collides with
+
+            CollisionLayer() = default;
+            CollisionLayer(uint16_t l, uint16_t m) : layer(l), mask(m) {}
+        };
+
+        // masks for collision layers
+        namespace CollisionLayers {
+            constexpr uint16_t NONE = 0;
+            constexpr uint16_t PLAYER = 1 << 0;          // 0x0001
+            constexpr uint16_t ENEMY = 1 << 1;           // 0x0002
+            constexpr uint16_t PLAYER_BULLET = 1 << 2;   // 0x0004
+            constexpr uint16_t ENEMY_BULLET = 1 << 3;    // 0x0008
+            constexpr uint16_t OBSTACLE = 1 << 4;        // 0x0010
+            constexpr uint16_t POWERUP = 1 << 5;         // 0x0020
+            constexpr uint16_t ALL = 0xFFFF;
+        }
+
+        struct CircleCollider : public IComponent {
+            float radius = 0.0f;
+
+            CircleCollider() = default;
+            CircleCollider(float r) : radius(r) {}
+        };
+
+        struct CollisionEvent : public IComponent {
+            Entity other = NULL_ENTITY;
+
+            CollisionEvent() = default;
+            CollisionEvent(Entity e) : other(e) {}
+        };
     }
 
 }
