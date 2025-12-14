32e68d4c827d0fbe0d3fcd74106c8067c797c3f4
erwan <erwan.seytor@epitech.eu>
2025-12-14 16:18:24 +0100

    fix(Collider): shrinked the player collider because it was too big and the game was too difficult

--- libs/engine/src/ECS/PlayerFactory.cpp
+++ libs/engine/src/ECS/PlayerFactory.cpp
@@ -23,10 +23,8 @@ namespace RType {
             registry.AddComponent<Controllable>(player, Controllable(200.0f));
 
             registry.AddComponent<Health>(player, Health(100, 100));
-
-            registry.AddComponent<BoxCollider>(player, BoxCollider(50.0f, 50.0f));
-
-            registry.AddComponent<CircleCollider>(player, CircleCollider(25.0f));
+            registry.AddComponent<BoxCollider>(player, BoxCollider(25.0f, 25.0f));
+            registry.AddComponent<CircleCollider>(player, CircleCollider(12.5f));
             registry.AddComponent<CollisionLayer>(player,
                                                   CollisionLayer(CollisionLayers::PLAYER,
                                                                  CollisionLayers::ENEMY | CollisionLayers::ENEMY_BULLET | CollisionLayers::OBSTACLE));
