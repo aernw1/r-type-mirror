78a75fff9d7733ba471601a1b6277454464fc9f3
keane <keane.adjobi@epitech.eu>
2026-01-14 17:55:17 +0100

    feat(engine): add audio support to PowerUpCollisionSystem

--- libs/engine/include/ECS/PowerUpCollisionSystem.hpp
+++ libs/engine/include/ECS/PowerUpCollisionSystem.hpp
@@ -10,6 +10,7 @@
 #include "ISystem.hpp"
 #include "CollisionDetectionSystem.hpp"
 #include "Renderer/IRenderer.hpp"
+#include "../Audio/IAudio.hpp"
 
 namespace RType {
     namespace ECS {
@@ -22,8 +23,11 @@ namespace RType {
             void Update(Registry& registry, float deltaTime) override;
             const char* GetName() const override { return "PowerUpCollisionSystem"; }
 
+            void SetPowerUpSound(Audio::SoundId id) { m_powerUpSound = id; }
+
         private:
             Renderer::IRenderer* m_renderer;
+            Audio::SoundId m_powerUpSound = Audio::INVALID_SOUND_ID;
         };
 
     }
